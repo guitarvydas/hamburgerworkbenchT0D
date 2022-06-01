e105cf5fd36d06e6c88d4d7dbdb69b62dd4cf3be
Paul Tarvydas <guitarvydas@gmail.com>
2022-06-01 17:37:49 -0400

    begin fmt.js

--- scripttranspile/fmt.js
+++ scripttranspile/fmt.js
@@ -1,1 +1,36 @@
+var argsarray = [];
+var fmtarray = [];
 
+exports.resetlists = function () {
+    argsarray = [];
+    fmtarray = [];
+    return "";
+}
+
+exports.pusharg = function (s) {
+    argsarray.push (s);
+    return "";
+}
+
+exports.pushfmt = function (s) {
+    fmtarray.push (s);
+    return "";
+}
+
+exports.args = function () {
+    var result = "";
+    argsarray.forEach (a => {
+	result += ` ${a} `;
+    });
+    return result;
+}
+exports.fmts = function () {
+    var result = "";
+    fmtarray.forEach (s => {
+	result += ` ${s} `;
+    });
+    return result;
+}
+
+
+   
